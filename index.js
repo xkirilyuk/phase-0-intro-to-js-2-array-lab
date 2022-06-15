e7747c2dcd74e15ae3a974313d1f13e1b3335892
Xenia <kakirilyuk@gmail.com>
2022-06-15 23:19:50 +0200

    commit tree 44200b5e7bb25daeed1cf0aa7f1f883b204bd893

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+
+
+function destructivelyAppendCat(name) {
+    name = "Ralph"
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    name = "Bob"
+    return cats.unshift(name);
+}
+
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+
+function appendCat(name) {
+    name = "Broom";
+    const appendCat = [...cats, name]
+    return appendCat;
+}
+
+
+function prependCat(name) {
+    name = "Arnold";
+    const prependCat = [name, ...cats];
+    return prependCat;
+}
+
+function removeLastCat() {
+    const removeLastCat = cats.slice(0, -1);
+    return removeLastCat;
+}
+
+function removeFirstCat() {
+    const removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
